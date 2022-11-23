aa892b8ef5b4f34dcac93d7ce19285a2ff17b08e
ultraloot <118390285+ultraloot@users.noreply.github.com>
2022-11-23 23:35:27 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -26,7 +26,7 @@ else:
             buttonz=ReplyKeyboardMarkup(
             [
                 ["start⚡️","help📚","DC"],
-                ["Subscribe ❤️","ping📡","status📊","maintainers😎"]
+                ["ping📡","status📊"]
                         
             ],
             resize_keyboard=True
